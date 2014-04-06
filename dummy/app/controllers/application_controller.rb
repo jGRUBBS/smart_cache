471dba2e9e6d6be6c10c0dc3520f80c5b41bdfed
Justin Grubbs <justin@jgrubbs.net>
2014-04-06 14:12:02 -0400

    update dummy app

--- dummy/app/controllers/application_controller.rb
+++ dummy/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  smart_caches :test_cache, :test_cache_two
+
+  def test_cache
+    puts "running test cache"
+  end
+
 end

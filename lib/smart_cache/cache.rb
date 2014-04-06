dbf2a4e3aa4fa5dbe42c4cfb9c5abe3c0144cf47
Justin Grubbs <justin@jgrubbs.net>
2014-04-06 14:10:40 -0400

    refactor cache class

--- lib/smart_cache/cache.rb
+++ lib/smart_cache/cache.rb
@@ -1,34 +1,48 @@
 module SmartCache
   class Cache
 
-    def fetch(key, *options, &block)
-      expires_in  = options.first[:expires_in]
-      read_or_write(key, expires_in, yield)
-      read(key)
+    def fetch(key, options = {}, &block)
+      @key, @expires_in = key, options[:expires_in]
+      read_or_write(yield)
     end
 
-    def read(key)
-      SmartCache.cache.read(key)
-    end
+    private
 
-    def write(key, value, expiry)
-      SmartCache.cache.write(key, value, expiry)
-    end
-
-    def read_or_write(key, expires_in, yield_content)
-      if SmartCache.cache.read(key)
-        # does not work with dalli but works with all other cache methods
-        cache_created_at = Time.at(SmartCache.cache.send(:read_entry, key, {}).created_at)
-        expiration_time  = cache_created_at + expires_in
-
-        if expiration_time < Time.now
-          SmartCache::Worker.perform_async( key, yield_content, (expires_in + 24.hours) )
-        end
-
-      else 
-        SmartCache.cache.write(key, yield_content, expires_in: expires_in + 24.hours )
+    def read_or_write(yield_content)
+      if val = read
+        schedule_async_action(yield_content) if expired?
+      else
+        val = write(yield_content, expires_in: @expires_in + 24.hours )
       end
+
+      val
     end
-  
+
+    def read
+      SmartCache.cache.read(@key)
+    end
+
+    def expired?
+      expires_at < Time.now
+    end
+
+    def expires_at
+      created_at + @expires_in
+    end
+
+    def created_at
+      # does not work with dalli but works with all other cache methods
+      Time.at(SmartCache.cache.send(:read_entry, @key, {}).created_at)
+    end
+
+    def schedule_async_action(yield_content)
+      SmartCache::Worker.perform_async( @key, yield_content, (@expires_in + 24.hours) )
+    end
+
+    def write(yield_content, expiry)
+      SmartCache.cache.write(@key, yield_content, expiry)
+      yield_content
+    end
+
   end
 end

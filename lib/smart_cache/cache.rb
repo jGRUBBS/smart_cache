54e58b81b44c333b593f824f302258650785c5e0
Justin Grubbs <justin@jgrubbs.net>
2014-04-10 03:18:07 -0400

    simplify cache module

--- lib/smart_cache/cache.rb
+++ lib/smart_cache/cache.rb
@@ -3,40 +3,21 @@ module SmartCache
 
     def fetch(key, options = {}, &block)
       @key, @expires_in = key, options[:expires_in]
-      read_or_write(yield)
+      unless val = read
+        schedule_async_action(yield)
+        val = write(yield, expires_in: @expires_in + SmartCache::TIME_PAD )
+      end
+      val
     end
 
     private
-
-    def read_or_write(yield_content)
-      if val = read
-        schedule_async_action(yield_content) if expired?
-      else
-        val = write(yield_content, expires_in: @expires_in + 24.hours )
-      end
-
-      val
-    end
 
     def read
       SmartCache.cache.read(@key)
     end
 
-    def expired?
-      expires_at < Time.now
-    end
-
-    def expires_at
-      created_at + @expires_in
-    end
-
-    def created_at
-      # does not work with dalli but works with all other cache methods
-      Time.at(SmartCache.cache.send(:read_entry, @key, {}).created_at)
-    end
-
     def schedule_async_action(yield_content)
-      SmartCache::Worker.perform_async( @key, yield_content, (@expires_in + 24.hours) )
+      SmartCache.processor.enqueue_smart_cache( @key, yield_content, @expires_in )
     end
 
     def write(yield_content, expiry)
